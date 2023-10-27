bb0a7a739e363ac92a025230f9c31b9d5bfd22c6
= <=>
2023-10-27 09:44:13 -0700

    fwd and rev

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# catco-data
-
-## CATegorical COherence benchmark dataset
+# CATegorical COherence benchmark dataset
 
 This is a dataset to test the coherence of LLMs following model edits with respect to the properties of categories and their members. An initial benchmark is made using the following animal categories and a set of properties about each different kind of animal.
 
@@ -15,8 +13,24 @@ This is a dataset to test the coherence of LLMs following model edits with respe
 | fish        | trout         | grouper    |
 | snake       | cobra         | Ninia      |
 
-
 For instance, one edit is: "A Holstein is a kind of dog". And one test is: "A sound a Holstein makes is __bark__" (originally "moo").
+
+## Creating the datasets
+
+Just run:
+
+```
+python3 build-datasets.py
+```
+
+## Test query structure
+
+The benchmark is multiple-choice with 2-4 choices for all queries. In light of the directionality of causal language models (predicting left to right), the dataset distinguishes between "forward" and "reverse" queries. A "forward" query is one where the edited subject is in the question prompt and an answer must be chosen. A "reverse" query is one where the edited subject is the anwer itself.
+
+- **Forward**: "A sound a Holstein makes is [bark / moo / tweet / hiss]"
+- **Reverse**: "Bark is a sound made by a [Holstein / Labrador / Siamese / Owlet]"
+
+**NOTE:** The edit evaluation dataset (`edits-evaluation.csv`) only tests properties that should be different following an edit.
 
 ## Project Structure
 
@@ -24,12 +38,7 @@ For instance, one edit is: "A Holstein is a kind of dog". And one test is: "A so
 - `animal-data.tsv`: properties of animal types
 - `build-datasets.py`: creates edit and benchmark `-evaluation` datasets (`baseline`` for unedited models and `edits` for edited)
 
-## Creating the dataset
-
-```
-python3 build-datasets.py
-```
-
-### requirements:
+### Requirements:
 
 - pandas
+- random
--- build-datasets.py
+++ build-datasets.py
@@ -1,21 +1,41 @@
 import pandas as pd
+import random
+
+random.seed(123)
 
 if __name__ == '__main__':
     
-    def proc_choices(df, baseline = False):
+    def proc_rev_choices(row):
+        tokens = (
+                types_df
+                .loc[lambda x: x.entity_type != row.orig_entity]
+                .melt(id_vars = ["entity_type"]) 
+            ).value.tolist()
+        
+        foils = random.choices([e for e in list(set(tokens)) if e != row.subj], k = 3)
+        
+
+        return [row.subj] + foils
+
+
+    def proc_fwd_choices(df, baseline = False):
+
         if baseline:
-            choice_list = df[["foil1", "foil2", "foil3"]].values.tolist()
+            fwd_choice_list = df[["foil1", "foil2", "foil3"]].values.tolist()
+            
         else:
-            choice_list = df[["foil1", "foil2", "foil3", "orig_answer_fwd"]].values.tolist()
+            fwd_choice_list = df[["foil1", "foil2", "foil3", "orig_answer_fwd"]].values.tolist()
         ans_list = df["answer_fwd"].tolist()
         out = []
 
-        for i in range(len(choice_list)):
-            distinct = list(set(choice_list[i]))
+        for i in range(len(fwd_choice_list)):
+            distinct = list(set(fwd_choice_list[i]))
             ans = ans_list[i]
             out.append([ans] + [c for c in distinct if c!=ans and pd.notna(c)])
 
-        df["choices"] = out
+        df["fwd_choices"] = out
+
+        
         return(df)
 
 
@@ -43,22 +63,27 @@ if __name__ == '__main__':
         .rename(columns = {'entity_type':'entity'})
         .melt(["entity"], value_name = 'subj')
         .merge(properties_df, on = 'entity')
-        .pipe(proc_choices, True)
+        .assign(orig_entity = lambda x: x.entity)
+        .pipe(proc_fwd_choices, True)
+        .assign(rev_choices = lambda x: x.apply(proc_rev_choices, 1))
+        .rename(columns = {"variable":"token_type"})
+        
     )
 
     baseline_df.to_csv("baseline-evaluation.csv")
     print("--- Wrote baseline evaluation csv file.")
 
     eval_df = (
-        pd.merge(
-            edits_df, 
-            properties_df.rename(columns = {"answer_fwd":"orig_answer_fwd", "answer_rev":"orig_answer_rev", "entity":"orig_entity"}), 
-            how="left", on = "orig_entity"
-            )
-            .merge(properties_df.filter(["entity", "answer_fwd", "answer_rev", "property"]), on = ["entity", "property"]) 
-            .loc[lambda x: x.orig_answer_fwd!=x.answer_fwd]
-            .pipe(proc_choices)
+    pd.merge(
+        edits_df, 
+        properties_df.rename(columns = {"answer_fwd":"orig_answer_fwd", "answer_rev":"orig_answer_rev", "entity":"orig_entity"}), 
+        how="left", on = "orig_entity"
+        )
+        .merge(properties_df.filter(["entity", "answer_fwd", "answer_rev", "property"]), on = ["entity", "property"]) 
+        .loc[lambda x: x.orig_answer_fwd!=x.answer_fwd]
+        .pipe(proc_fwd_choices)
+        .assign(rev_choices = lambda x: x.apply(proc_rev_choices, 1))
+        .rename(columns = {"variable":"token_type"})
     )
-
     eval_df.to_csv("edits-evaluation.csv")
     print("--- Wrote edits evaluation csv file.")
