7a103a75e6338ab4bb090acb7bf6d78be5a46995
= <=>
2023-11-03 12:40:29 -0700

    new evals, bug fixes

--- build-datasets.py
+++ build-datasets.py
@@ -12,8 +12,7 @@ if __name__ == '__main__':
                 .melt(id_vars = ["entity_type"]) 
             ).value.tolist()
         
-        foils = random.choices([e for e in list(set(tokens)) if e != row.subj], k = 3)
-        
+        foils = random.sample([e for e in list(set(tokens)) if e != row.subj], k = 3) # sample WITHOUT replacement
 
         return [row.subj] + foils
 
@@ -41,6 +40,12 @@ if __name__ == '__main__':
 
     types_df = pd.read_csv("animal-type-tokens.tsv", sep="\t")
     properties_df = pd.read_csv("animal-data.tsv", sep="\t")
+
+    longtypes_df = (
+        types_df
+        .melt(["entity_type"], var_name = "token_type", value_name = "subj")
+        
+    )
 
     edits_df = (
         pd.merge(types_df, types_df, how = "cross")
@@ -70,6 +75,60 @@ if __name__ == '__main__':
         
     )
 
+    # baseline_cat_members = (
+    #     types_df
+    #     .melt(["entity_type"], var_name = "token_type", value_name = "subj")
+    #     .assign(
+    #         query_fwd =  "a <subj> is a <answer>",
+    #         query_rev = "one example of a <answer> is a <subj>",
+    #         answer_fwd = lambda x: x.entity_type,
+    #         answer_rev = lambda x: x.subj,
+    #         orig_entity = lambda x: x.entity_type,
+    #         entity = lambda x: x.entity_type,
+    #         property = "category_membership",
+    #         fwd_choices = lambda d: d.apply(lambda x: [x.entity_type] + [t for t in types_df.entity_type.to_list() if t != x.entity_type], 1),
+    #         rev_choices = lambda d: d.apply(lambda x: [x.subj] + [t for t in d.loc[d.entity_type != x.entity_type].subj.to_list()], 1)
+    #         )
+    # )
+
+    baseline_cat_members = (
+        longtypes_df
+        .assign(
+            category_membership = "a <subj> is a <answer>",
+            category_membership1 = "which is where the name originates. In any case, a <subj> is a kind of <answer>",
+            category_membership2 = "it is correct to say that any <subj> is a <answer>",
+            category_membership3 = "Answer key:\n\nAnswer 1: D) a <subj> is one kind of <answer>"
+            )
+        .melt(id_vars = ["entity_type", "token_type", "subj"],  var_name = "property", value_name = "query_fwd")
+        .assign(
+            query_rev = longtypes_df
+                        .assign(
+                            category_membership = "one kind of <answer> is a <subj>",
+                                    category_membership1 = "which is where the name originates. In any case, one kind of <answer> is a <subj>",
+                                    category_membership2 = "it is correct to say that one example of a <answer> is a <subj>",
+                                    category_membership3 = "Answer key:\n\nAnswer 1: D. Among these choices, the member of the category <answer> is <subj>"
+                            )
+                        .melt(id_vars = ["entity_type", "token_type", "subj"], var_name = "property", value_name = "query_rev")
+                        .query_rev
+        )
+        .assign(
+            fwd_choices = lambda d: d.apply(lambda x: [x.entity_type] + [t for t in types_df.entity_type.to_list() if t != x.entity_type], 1),
+            rev_choices = lambda d: d.apply(lambda x: [x.subj] + [t for t in d.loc[d.entity_type != x.entity_type].subj.to_list()], 1)
+        )
+    )
+
+    
+
+    baseline_category_property_df = (
+        baseline_df
+        .assign(subj = lambda x: x.entity)
+        .assign(rev_choices = lambda d: d.apply(lambda x: longtypes_df.loc[longtypes_df.subj.isin(x.rev_choices)].entity_type.to_list(), 1)) #longtypes_df.loc[longtypes_df.subj.isin(x.rev_choices)].entity_type)
+        .drop_duplicates(subset = ["entity", "property"])
+        .assign(token_type = "entity")
+    )
+
+    baseline_df = pd.concat([baseline_cat_members, baseline_category_property_df, baseline_df])
+
     baseline_df.to_csv("baseline-evaluation.csv")
     print("--- Wrote baseline evaluation csv file.")
 
@@ -85,5 +144,49 @@ if __name__ == '__main__':
         .assign(rev_choices = lambda x: x.apply(proc_rev_choices, 1))
         .rename(columns = {"variable":"token_type"})
     )
+
+    # eval_cat_members = (
+    #     edits_df
+    #     .assign(
+    #             query_fwd =  "a <subj> is a <answer>",
+    #             query_rev = "one example of a <answer> is a <subj>",
+    #             answer_fwd = lambda x: x.entity,
+    #             answer_rev = lambda x: x.subj,
+    #             # orig_entity = lambda x: x.entity,
+    #             entity = lambda x: x.entity,
+    #             property = "category_membership",
+    #             fwd_choices = lambda d: d.apply(lambda x: [x.entity] + [t for t in types_df.entity_type.to_list() if t != x.entity], 1),
+    #             rev_choices = lambda d: d.apply(lambda x: [x.subj] + [t for t in d.loc[(d.entity != x.entity) & (d.orig_entity != x.entity)].subj.to_list()], 1)
+    #     )
+    # )
+
+    eval_cat_members = (
+    edits_df
+    .assign(
+        category_membership = "a <subj> is a <answer>",
+        category_membership1 = "which is where the name originates. In any case, a <subj> is a kind of <answer>",
+        category_membership2 = "it is correct to say that any <subj> is a <answer>",
+        category_membership3 = "Answer key:\n\nAnswer 1: D) a <subj> is one kind of <answer>"
+        )
+    .melt(id_vars = ["entity", "orig_entity", "variable", "edit", "subj"],  var_name = "property", value_name = "query_fwd")
+    .assign(
+        query_rev = edits_df
+                    .assign(
+                        category_membership = "one kind of <answer> is a <subj>",
+                                category_membership1 = "which is where the name originates. In any case, one kind of <answer> is a <subj>",
+                                category_membership2 = "it is correct to say that one example of a <answer> is a <subj>",
+                                category_membership3 = "Answer key:\n\nAnswer 1: D. Among these choices, the member of the category <answer> is <subj>"
+                        )
+                    .melt(id_vars = ["entity", "orig_entity", "variable", "edit", "subj"],  var_name = "property", value_name = "query_rev")
+                    .query_rev
+    )
+    .assign(
+            fwd_choices = lambda d: d.apply(lambda x: [x.entity] + [t for t in types_df.entity_type.to_list() if t != x.entity], 1),
+            rev_choices = lambda d: d.apply(lambda x: [x.subj] + [t for t in d.loc[d.entity != x.entity].subj.to_list()], 1)
+        )
+)
+
+        
+    eval_df = pd.concat([eval_cat_members, eval_df])    
     eval_df.to_csv("edits-evaluation.csv")
     print("--- Wrote edits evaluation csv file.")
